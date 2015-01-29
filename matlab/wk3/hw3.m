94face00d439f3b449452d25501fc97dba0d1906
Paul R. Adams <paul.r.adams4@boeing.com>
2015-01-28 20:11:20 -0800

    hw3

--- matlab/wk3/hw3.m
+++ matlab/wk3/hw3.m
@@ -6,11 +6,11 @@ syms t
 A = [-7, -12; 1, 0];
 B = [1; 0];
 C = [-4, -10];
-D = 0;
+D = 1;
 x0 = [1; 2];
 
 %% Solution to part a
-pretty(expm(A*t)*x0)
+Phi = expm(A*t);
 
 %% Solution to part b
 sys = ss(A, B, C, D);
